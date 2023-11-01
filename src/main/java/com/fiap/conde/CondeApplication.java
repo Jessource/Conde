d4d74be2c405d25c04f40c5352ff223231834fc8
xuxu <jsdl.9991@gmail.com>
2023-11-01 11:30:28 -0300

    inicializando projeto spring boot

---
+++ src/main/java/com/fiap/conde/CondeApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.conde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/conde/CondeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.conde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
